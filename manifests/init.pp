33c0e6f164bb606efd4ac51cadbab4dc554f8fa6
joeypiccola <joey.piccola@gmail.com>
2016-09-13 11:34:07 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,11 @@
 
 class users {
     user {'elmo':
-    ensure   => 'present',
-    comment  => 'this is joeys user',
-    password => 'ThisIsMyPassword123****',
+    ensure      => 'present',
+    comment     => 'this is joeys user',
+    password    => 'ThisIsMyPassword123****',
+    home        => 'c:/tmp/elmoHD',
+    managehome  => true,    
   }
 
 group { 'muppet':
